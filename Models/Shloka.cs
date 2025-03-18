b8c77638527c2e7667185d743028aed3f11905d5
Aasheeta Gajera <aasheetagajera03@gmail.com>
2025-03-18 10:18:26 +0530

    Aasheeta

---
+++ Models/Shloka.cs
@@ -0,0 +1,8 @@
+public class Shloka
+{
+    public int Id { get; set; }
+    public string ChapterName { get; set; }
+    public int TotalShlokas { get; set; }
+    public int ShlokaNumber { get; set; }
+    public string ShlokaText { get; set; }
+}
