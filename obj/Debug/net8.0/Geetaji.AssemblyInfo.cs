1ccf1bf4ad3f3656fbc0570444d6390397b567dd
Aasheeta Gajera <aasheetagajera03@gmail.com>
2025-04-14 19:07:43 +0530

    aashheta

--- obj/Debug/net8.0/Geetaji.AssemblyInfo.cs
+++ obj/Debug/net8.0/Geetaji.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Geetaji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac6c82ce958a4255b8dbe1f73bdb73e7745227c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a567c72804fd6fd38372c1775305b67ce41372")]
 [assembly: System.Reflection.AssemblyProductAttribute("Geetaji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Geetaji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
